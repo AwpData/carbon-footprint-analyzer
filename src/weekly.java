6e7bd2a71ce1ff2f3ed8a4115c4c195f1a7fddbf
Ncumic <Ncumic00@gmail.com>
2022-10-22 14:21:33 -0700

    weekly.java

--- src/weekly.java
+++ src/weekly.java
@@ -1,7 +1,10 @@
 import java.util.*;
-
+import java.util;
 public class Weekly{
     private Map<String,ArrayList<Integer>> weeklyLog = new HashMap<>();
 
+    public static void builtWeekly() {
+        for
+    }
 
 }
