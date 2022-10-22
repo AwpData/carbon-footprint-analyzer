62dd91313dda20f21c229c47eaf4eb420f4edcef
Ncumic <Ncumic00@gmail.com>
2022-10-22 15:52:47 -0700

    daily.java

--- src/daily.java
+++ src/daily.java
@@ -4,8 +4,16 @@ import java.util;
 public class Daily{
     private ArrayList<Integer> dailyLog = new ArrayList<>();
 
+
     public static returnLogs(){
-        dailyLog.add(5);
+        dailyLog.add(0,KMDriven());
     }
+
+    public static int KMDriven(int KM) {
+
+    }
+
+
+
 
 }
--- src/weekly.java
+++ src/weekly.java
@@ -1,10 +1,15 @@
 import java.util.*;
 import java.util;
-public class Weekly{
+public class Weekly {
     private Map<String,ArrayList<Integer>> weeklyLog = new HashMap<>();
 
-    public static void builtWeekly() {
-        for
+    public static void main(String[] args) {
+        return shower();
+
+    }
+
+    public static int shower() {
+        return 5;
     }
 
 }
