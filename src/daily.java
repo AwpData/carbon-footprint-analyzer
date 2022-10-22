dc7cc8c1a3c76059b7add92590475f1423212fd6
Daniela Cuevas <danic@MacBook-Pro-10.local>
2022-10-22 14:37:20 -0700

    Added daily

---
+++ src/daily.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util;
+
+public class Daily{
+    private ArrayList<Integer> dailyLog = new ArrayList<>();
+
+    public static returnLogs(){
+        dailyLog.add(5);
+    }
+
+}
