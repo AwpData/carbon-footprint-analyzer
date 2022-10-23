4ab2b216f153d60056d2b9aee0c25256903bcb5b
Daniela Cuevas <danic@MacBook-Pro-10.local>
2022-10-22 20:17:02 -0700

    attempt at sql

---
+++ src/test.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Vehicles (
+  Make VARCHAR(200),
+  Model VARCHAR(200),
+  Vehicle_Class VARCHAR(200),
+  Engine_Size DECIMAL,
+  Cylinders INTEGER,
+  Transmission VARCHAR(10),
+  Fuel_type VARCHAR(10),
+  Fuel_consumption_city DECIMAL,
+  Fuel_consumption_hwy DECIMAL,
+  Combined_fuel_consumption DECIMAL,
+  Fuel_consumption_mpg DECIMAL,
+  Co2_emissions DECIMAL
+); 
